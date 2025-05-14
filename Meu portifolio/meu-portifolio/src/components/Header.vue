f81f2dc001df8df80c24d32a0974fbc045115e06
Luiz_Buarque_Dev <luizgabrielbuarque@gmail.com>
2025-05-14 14:43:58 -0300

    ajustes header

--- Meu portifolio/meu-portifolio/src/components/Header.vue
+++ Meu portifolio/meu-portifolio/src/components/Header.vue
@@ -3,14 +3,14 @@
 </script>
 
 <template>
-    <header class="d-flex">
-        <aside>
-            <h1>&lt;LG /&gt;</h1>
+    <header class="d-flex justify-content-between align-items-center">
+        <aside class="ms-5">
+            <router-link to="/">&lt;LG /&gt;</router-link>
         </aside>
-        <h1></h1>
-        <ul>
+
+        <ul class="d-flex list-unstyled me-5 gap-5">
             <li><router-link to="/"> Home </router-link></li>
-            <li><router-link to="/"> Home </router-link></li>
+            <li><router-link to="/"> Github </router-link></li>
         </ul>
     </header>
 </template>
@@ -18,10 +18,21 @@
 <style lang="scss" scoped>
 header {
     background: var(--gradient_1);
+    
+    a {
+        text-decoration: none;
+        color: var(--color_5);
+    }
 
     aside {
         font-family: "Share Tech", sans-serif;
         font-size: 2.5em;
     }
+
+    ul {
+  margin: 0;
+  padding: 0;
+}
+
 }
 </style>
--- Meu portifolio/meu-portifolio/src/main.js
+++ Meu portifolio/meu-portifolio/src/main.js
@@ -6,9 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import { createBootstrap } from 'bootstrap-vue-next'
+
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(createBootstrap())
 app.use(router)
 
 app.mount('#app')

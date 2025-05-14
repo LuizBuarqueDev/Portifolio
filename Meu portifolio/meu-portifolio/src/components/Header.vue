9a436015cb996822aca27a49f4de03a028c24d3e
Luiz_Buarque_Dev <luizgabrielbuarque@gmail.com>
2025-05-14 10:54:53 -0300

    Header incompleto

---
+++ Meu portifolio/meu-portifolio/src/components/Header.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <aside>
+            <img src="/icon sem fundo.png" alt="Logo <LG/ >">
+        </aside>
+        <h1></h1>
+        <ul>
+            <li><router-link to="/"> Home </router-link></li>
+            <li><router-link to="/"> Home </router-link></li>
+        </ul>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header{
+    background: var(--gradient_1);
+
+    img {
+        height: 100px;
+        width: 100px;
+    }
+}
+</style>

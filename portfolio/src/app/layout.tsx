168ab6df1a6323d49af22a013d1c47cb4af061d8
Luiz_Buarque_Dev <luizgabrielbuarque@gmail.com>
2025-08-06 20:29:34 -0300

    Pequena correção

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./styles/globals.css";
 
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -35,7 +38,9 @@ export default function RootLayout({
       </head>
 
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,15 +1,11 @@
-import { Footer } from "@/components/Foooter";
-import { Header } from "@/components/Header";
 import { Presentation } from "@/components/Presentation";
 
 export default function Home() {
   return (
     <div>
-        <Header />
       <main className="flex">
         <Presentation />
       </main>
-      <Footer />
     </div>
   );
 }

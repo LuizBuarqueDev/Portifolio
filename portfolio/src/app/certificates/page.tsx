5cc0f3eb5767942ebd3f14ba1b990beddd6daaee
Luiz_Buarque_Dev <luizgabrielbuarque@gmail.com>
2025-08-07 12:30:54 -0300

    Pagina de certificados

---
+++ portfolio/src/app/certificates/page.tsx
@@ -0,0 +1,9 @@
+export default function CertificatesPage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <h1 className="text-4xl font-bold mb-8">Certificados</h1>
+            <p className="text-lg mb-4">Em breve, esta página estará disponível com todos os meus certificados.</p>
+            <p className="text-sm text-gray-500">Obrigado pela sua paciência!</p>
+        </div>
+    );
+}
--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export const Header = () => {
   return (
     <header className="flex justify-between items-center p-4">
-      <h3 className="hidden sm:inline">@LuizBuarqueDev</h3>
+      <Link href={"/"} className="flex items-center gap-2">
+        <h3 className="hidden sm:inline">@LuizBuarqueDev</h3>
+      </Link>
       <nav>
         <ul className="flex gap-10 items-center [&_i]:text-2xl [&_li]:transition-transform [&_li]:hover:scale-110 [&_li]:hover:border-b-2 [&_li]:hover:border-white">
           <li>
@@ -21,6 +25,12 @@ export const Header = () => {
               <i className="fa-solid fa-envelope"></i>
               <span className="hidden sm:inline">E-mail</span>
             </a>
+          </li>
+          <li>
+            <Link href="/certificates" className="flex items-center gap-2">
+              <i className="fa-solid fa-certificate"></i>
+              <span className="hidden sm:inline">Certificados</span>
+            </Link>
           </li>
         </ul>
       </nav>

d57bdcd197f410cecc2fce25155aec2dc9db989c
Luiz_Buarque_Dev <luizgabrielbuarque@gmail.com>
2025-08-06 23:45:52 -0300

    ajuste na cor do botão do card

--- portfolio/src/components/ProjectCards.tsx
+++ portfolio/src/components/ProjectCards.tsx
@@ -15,13 +15,18 @@ export const ProjectCards = () => {
                         <a href="#">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Prateleira Inteligente</h5>
                         </a>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        <p className="mb-3 font-normal text-white">
                             O projeto foi criado com o objetivo de conectar leitores apaixonados. Aqui, você pode dar notas aos livros que leu, escrever comentários sinceros sobre sua experiência e conferir o que outras pessoas acharam da mesma leitura. É uma forma simples e colaborativa de descobrir novos títulos, trocar opiniões e enriquecer seu repertório literário. Cada avaliação ajuda a comunidade a encontrar boas histórias e evitar decepções!
                         </p>
-                        <a href="https://github.com/carlla15/PrateleiraInteligente" target="_blank" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <a
+                            href="https://github.com/carlla15/PrateleiraInteligente"
+                            target="_blank"
+                            className="inline-flex items-center px-4 py-2 text-sm font-semibold text-white bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-1 transition-colors duration-200"
+                        >
                             View Code
                             <i className="fa-solid fa-arrow-right ml-2"></i>
                         </a>
+
                     </div>
                 </div>
 

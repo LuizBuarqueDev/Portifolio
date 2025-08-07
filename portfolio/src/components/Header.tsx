2590566443886933946cf708a5fa9608624d81b5
Luiz_Buarque_Dev <luizgabrielbuarque@gmail.com>
2025-08-07 01:18:43 -0300

    ajustes de responsividade

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -1,23 +1,25 @@
 export const Header = () => {
   return (
     <header className="flex justify-between items-center p-4">
-      <h3 className="text-2xl font-bold">@LuizBuarqueDev</h3>
+      <h3 className="hidden sm:inline">@LuizBuarqueDev</h3>
       <nav>
-        <ul className="flex gap-10 items-center [&_li]:transition-transform [&_li]:hover:scale-110 [&_li]:hover:border-b-2 [&_li]:hover:border-white">
+        <ul className="flex gap-10 items-center [&_i]:text-2xl [&_li]:transition-transform [&_li]:hover:scale-110 [&_li]:hover:border-b-2 [&_li]:hover:border-white">
           <li>
-            <a href="https://github.com/LuizBuarqueDev" target="_blank" rel="noopener noreferrer">
-              <i className="fa-brands fa-github"></i> Github
+            <a href="https://github.com/LuizBuarqueDev" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2">
+              <i className="fa-brands fa-github"></i>
+              <span className="hidden sm:inline">Github</span>
             </a>
           </li>
           <li>
-
-            <a href="https://www.linkedin.com/in/luiz-gabriel-buarque-vasconcelos-24074328b/" target="_blank" rel="noopener noreferrer">
-              <i className="fa-brands fa-linkedin"></i> Linkedin
+            <a href="https://www.linkedin.com/in/luiz-gabriel-buarque-vasconcelos-24074328b/" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2">
+              <i className="fa-brands fa-linkedin"></i>
+              <span className="hidden sm:inline">Linkedin</span>
             </a>
           </li>
           <li>
-            <a href="mailto:luizgabrielbuarque@gmail.com" target="_blank" rel="noopener noreferrer">
-              <i className="fa-solid fa-envelope"></i> E-mail
+            <a href="mailto:luizgabrielbuarque@gmail.com" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2">
+              <i className="fa-solid fa-envelope"></i>
+              <span className="hidden sm:inline">E-mail</span>
             </a>
           </li>
         </ul>
